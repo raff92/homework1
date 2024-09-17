aa3838c16ac09eeda534e9452305b284ea16a2fd
raff92 <macho.vaganyan75@mail.ru>
2024-09-17 19:39:19 +0300

    Add files via upload

---
+++ homework.js
@@ -0,0 +1,142 @@
+// 1 Задание
+let number = 123
+let number1 = 35
+let number2 = 70
+
+if (number === 123) {
+    console.log("odd")
+} else { 
+    console.log ("even")
+}
+
+//
+
+if (number1 === 35) {
+    console.log("odd")
+} else { 
+    console.log ("even")
+}
+
+//
+
+if (number2 === 70 ) {
+    console.log("even")
+} else { 
+    console.log ("odd")
+}
+
+// 2 Задание
+
+let number = 3
+let number1 = 14
+
+if ((number / 3) && (number1 / 14)) {
+    console.log (0) 
+} else { 
+    console.log (1) 
+}
+
+//
+
+let number = 18
+let number1 = 2
+
+if ((number / 2) && (number1 / 18)) {
+    console.log (1) 
+} else { 
+    console.log (0) 
+}
+
+//
+
+let number = 7
+let number1 = 21
+
+if ((number / 21 ) && (number1 / 7)) {
+    console.log (1) 
+} else { 
+    console.log (0) 
+}
+
+// 3 Задание
+
+let a = 45
+let b = 90
+
+180 - ( a + b )
+ //   
+let a = 30
+let b = 30
+
+180 - ( a + b )
+
+//
+
+let a = 75 
+let b = 25
+
+180 - ( a + b )
+
+// 4 Задание
+
+( 45 + -12 + 0 + 3 + -15 ) / 4,2
+
+( 7 + 52 + -23 + 9 + -81 ) / -7,2
+
+// 5 Задание 
+
+switch ((21 % 3) && (21 / 7)) {
+    case 0: {
+        console.log(0)
+        break; 
+    } default: {
+        console.log(1)
+    }
+}
+
+//
+
+switch (( 35 % 5 ) && ( 35 % 7 )) {
+    case 0: {
+        console.log(0)
+        break;
+    } default: {
+        console.log(1)
+    }
+}
+
+//
+
+switch ((13 % 3) && (13 % 5) && (13 % 7)) {
+    case 0: {
+        cosole.log(0) 
+        break;
+    } default: { 
+        console.log(1)
+    }
+}
+
+//
+
+switch ((420 % 3) && (420 % 5) && (420 % 7)) {
+    case 0: {
+        console.log(0) 
+        break;
+    } default: { 
+        console.log(1)
+    }
+}
+
+//
+
+switch (24 % 3) {
+    case 0: {
+        console.log(0) 
+        break;
+    } default: { 
+        console.log(1)
+    }
+}
+
+
+
